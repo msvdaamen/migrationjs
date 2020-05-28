a7ead68113f5f9c04785f822d7527b1d49878237
Mischa <msvdaamen@outlook.com>
2020-05-28 17:02:23 +0200

    Generate conf file

--- bin/migrationjs.js
+++ bin/migrationjs.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const args = require('process.args')();
 const {migrate} = require('../src/index');
+var fs = require('fs');
 
 if (args.hasOwnProperty('migrate')) {
     const config = require(process.cwd() + '/migrationjs.conf.json');
@@ -10,6 +11,23 @@ if (args.hasOwnProperty('migrate')) {
     migrate(process.cwd(), config).then(() => {
         process.exit();
     });
+} else if (args.hasOwnProperty('generate:config')) {
+    const config = require(process.cwd() + '/migrationjs.conf.json');
+    if (!config) {
+        const newConfig = {
+            database: {
+                host: 'localhost',
+                user: 'root',
+                password: '',
+                database: ''
+            },
+            folderName: 'migrations'
+        };
+        const json = JSON.stringify(newConfig);
+        fs.writeFile(process.cwd() + '/migrationjs.conf.json', json, 'utf8', () => {});
+    } else {
+        throw Error('There is already an migrationjs.conf.json file');
+    }
 } else {
     process.exit();
 }
